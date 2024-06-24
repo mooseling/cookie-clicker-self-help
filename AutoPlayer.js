e6e6cfd481d7fca07a15c69e2dcaefc98928a021
James Stevenson <mooseling@hotmail.com>
2024-06-24 10:57:28 +0100

    Create initial AutoPlayer

---
+++ AutoPlayer.js
@@ -0,0 +1,83 @@
+class AutoPlayer {
+
+    autoClick = true;
+    autoClickShimmers = true;
+    autoPledge = true;
+    autoChristmas = true;
+
+    #running;
+    #fastLoopTimeout;
+    #slowLoopTimeout;
+
+
+    constructor() {
+        this.#running = false;
+    }
+
+
+    run() {
+        if (this.#running)
+            return;
+
+        this.#running = true;
+
+        this.#fastLoop();
+        this.#slowLoop();
+    }
+
+
+    stop() {
+        this.#running = false;
+        
+        clearTimeout(this.#fastLoopTimeout);
+        clearTimeout(this.#slowLoopTimeout);
+    }
+
+
+    #fastLoop() {
+        if (!this.#running)
+            return;
+
+        if (this.autoClick)
+            Game.ClickCookie();
+
+        this.#fastLoopTimeout = setTimeout(this.#fastLoop.bind(this), 0);
+    }
+
+
+    #slowLoop() {
+        if (!this.#running)
+            return;
+
+        if (this.autoPledge)
+            this.#pledgeToTheElders(); // It's safe to call this often, it will check if it's needed
+        if (this.autoClickShimmers)
+            this.#clickShimmers();
+        if (this.autoChristmas)
+            this.#stayInChristmas();
+
+        this.#slowLoopTimeout = setTimeout(this.#slowLoop.bind(this), 5000);
+    }
+
+
+    #pledgeToTheElders() {
+        const elderPledgeButton = document.querySelector('.upgrade[data-id="74"]');
+        if (elderPledgeButton.classList.contains('enabled'))
+            elderPledgeButton.click();
+    }
+
+
+    // Shimmers are golden cookies and reindeer. We want to auto-click both.
+    #clickShimmers() {
+        const shimmers = document.getElementById('shimmers').children;
+        for (const shimmer of shimmers)
+            shimmer.click();
+    }
+
+
+    #stayInChristmas() {
+        const christmasSwitch = document.querySelector('.upgrade[data-id="182"]');
+        if (christmasSwitch.classList.contains('enabled'))
+            christmasSwitch.click();
+    }
+}
