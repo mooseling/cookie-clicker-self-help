b949c40e79c4d22d9ce835154910a9b45a119705
James Stevenson <mooseling@hotmail.com>
2024-06-24 11:54:39 +0100

    Create snippets.js

---
+++ snippets.js
@@ -0,0 +1,5 @@
+// Paste in the class, then use this to get it running
+autoPlayer = new AutoPlayer();autoPlayer.run();
+
+// For the stock market, I'm using an addon. Load it thusly:
+Game.LoadMod('https://klattmose.github.io/CookieClicker/IdleTrading.js');
