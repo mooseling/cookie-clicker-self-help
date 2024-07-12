49bba08fa7647e1fc5e39c96c319775909282274
James Stevenson <mooseling@hotmail.com>
2024-07-12 09:05:24 +0100

    Add snippet for garden helper addon

--- snippets.js
+++ snippets.js
@@ -3,3 +3,6 @@ autoPlayer = new AutoPlayer();autoPlayer.run();
 
 // For the stock market, I'm using an addon. Load it thusly:
 Game.LoadMod('https://klattmose.github.io/CookieClicker/IdleTrading.js');
+
+// Gardening addon:
+Game.LoadMod('https://rawgit.com/yannprada/cookie-garden-helper/master/cookie-garden-helper.js');
