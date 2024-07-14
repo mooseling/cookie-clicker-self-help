ecde083421e6c9bf8143a0dd4b75937c5a3ed659
James Stevenson <mooseling@hotmail.com>
2024-07-14 19:08:41 +0100

    Add Horticookie to snippets

--- snippets.js
+++ snippets.js
@@ -4,5 +4,8 @@ autoPlayer = new AutoPlayer();autoPlayer.run();
 // For the stock market, I'm using an addon. Load it thusly:
 Game.LoadMod('https://klattmose.github.io/CookieClicker/IdleTrading.js');
 
-// Gardening addon:
+// Basic garden helper. Shows plants yet to be unlocked, and has some simple harvesting options
+Game.LoadMod('https://klattmose.github.io/CookieClicker/Horticookie.js')
+
+// More complex garden helper. Does auto-plant and has more config options for auto-harvest
 Game.LoadMod('https://rawgit.com/yannprada/cookie-garden-helper/master/cookie-garden-helper.js');
