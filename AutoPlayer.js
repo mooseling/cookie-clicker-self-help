55ae16762927444d7805fc9fc82470559ce6bd7d
James Stevenson <mooseling@hotmail.com>
2026-09-06 15:22:22 +0100

    Swich Godzamok strategy to repeated farm selling

--- AutoPlayer.js
+++ AutoPlayer.js
@@ -14,6 +14,7 @@ class AutoPlayer {
     fastLoopTime = 20; // We used to do 0, but the page is freezing. Possibly from other extensions. Anyway, nice to control this rate.
     godzamokFarmCountNormal = 950;
     godzamokFarmCountEndGame = 1000;
+    farmSellAmount = 2000;
 
     saves = [];
     shimmersClicked = 0;
@@ -249,14 +250,8 @@ class AutoPlayer {
             else
                 targetFarmCount = this.godzamokFarmCountNormal;
         }
-        const targetMineCount = targetFarmCount - 50
-        const targetFactoryCount = targetMineCount - 50
 
-        // The GUI only shows the buff from selling the farms, but that's only because the GUI doesn't update!
-        // Selling more buildings DOES increase the multiplier!
         const farm = Game.Objects.Farm;
-        const mine = Game.Objects.Mine;
-        const factory = Game.Objects.Factory;
 
         // If the game is in sell-mode, it won't buy things. So we'll just switch it over briefly...
         let inSellMode = Game.buyMode === -1;
@@ -265,18 +260,11 @@ class AutoPlayer {
 
         if (farm.amount < targetFarmCount)
             farm.buy(targetFarmCount - farm.amount);
-        if (mine.amount < targetMineCount - 50)
-            mine.buy(targetMineCount - mine.amount);
-        if (factory.amount < targetFactoryCount - 50)
-            factory.buy(targetFactoryCount - factory.amount);
 
-        farm.sell(-1); // -1 means all
-        mine.sell(-1);
-        factory.sell(-1);
-
-        farm.buy(targetFarmCount);
-        mine.buy(targetMineCount);
-        factory.buy(targetFactoryCount);
+        for (let i = 0; i < this.farmSellAmount; i++) {
+            farm.sell(-1); // -1 means all
+            farm.buy(targetFarmCount);
+        }
 
         if (inSellMode)
             Game.buyMode = -1; // ...and reset it afterwards
